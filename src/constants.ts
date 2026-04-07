a8017e52ac04d6c495f2b84bbc985adcb146de19
Demetri Bis <26793601+zerthmonk@users.noreply.github.com>
2026-04-07 22:45:38 +0200

    add: full site structure

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+import { buildUrl } from "./scripts/path-utils";
+
+const LINKEDIN_URL = "https://www.linkedin.com/in/olgabis";
+const TG_URL = "https://t.me/shuniyata";
+const CV_URL = buildUrl("/downloads/Bis_Olga_Senior_UX_UI_designer_cv.pdf");
+
+const DEFAULT_OG_IMAGE_PATH = "/images/social/olga-bis-share-v2.jpg";
+const PERSON_IMAGE_PATH = DEFAULT_OG_IMAGE_PATH;
+
+export {
+  CV_URL,
+  DEFAULT_OG_IMAGE_PATH,
+  LINKEDIN_URL,
+  PERSON_IMAGE_PATH,
+  TG_URL,
+};
