bb25a31d36c783996d57d5a02585eee38767214a
Olga Shunyata <265104768+olya-shunyata@users.noreply.github.com>
2026-05-11 21:13:13 +0200

    changed cv

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { buildUrl } from "./scripts/path-utils";
 
 const LINKEDIN_URL = "https://www.linkedin.com/in/olgabis";
 const TG_URL = "https://t.me/shuniyata";
-const CV_URL = buildUrl("/downloads/Bis_Olga_Senior_UX_UI_designer_cv.pdf");
+const CV_URL = buildUrl("/downloads/Bis_Olga_UX_UI_designer_cv.pdf");
 
 const DEFAULT_OG_IMAGE_PATH = "/images/social/olga-bis-share-v2.jpg";
 const PERSON_IMAGE_PATH = DEFAULT_OG_IMAGE_PATH;
